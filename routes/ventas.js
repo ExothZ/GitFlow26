60e4e4303ebc78718cfe5da20e33d99393b34574
UwaldoVillatoro26 <uwualdoleo@gmail.com>
2022-02-13 19:40:06 -0600

    Ventas añadido

---
+++ routes/ventas.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const jwt = require('jwt-simple');
+const UserService = require('../services/activate-user');
+
+function userApi(app) {
+    const router = express.Router();
+    app.use("/api/activate-user", router);
+    console.log();
+
+    const userService = new UserService();
+    console.log();
+
+    router.put("/", async function (req, res, next) {
+        const { body: user } = req;
+        console.log();
+        try {
+            const usr = await userService.getUser(user.id);
+            if (usr) {
+                const decoded = jwt.decode(usr.token, 'dog');
+                if (decoded.exp && decoded.sub === user.tk) {
+                    usr.status = true;
+                    const updateUsr = await userService.updateUser(usr, usr._id);
+                    res.status(200).json({
+                        data: updateUsr,
+                        example: updateUsr,
+                        message: 'user selected successfully'
+                    });
+                    console.log();
+                }
+            }
+        } catch (err) {
+            next(err);
+            console.log();
+        }
+    });
+
+}
+
+module.exports = userApi;
